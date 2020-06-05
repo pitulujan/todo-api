87010c0bed995ffd6ec7b26b5680d4cbbf5548ef
pitulujan <lujanmartin91@gmail.com>
2020-06-05 12:38:42 -0300

    Added Schemas and Examples to Swagger Doc

--- api/flasgger/api_flasgger.py
+++ api/flasgger/api_flasgger.py
@@ -20,9 +20,83 @@ def get_tasks():
     return jsonify({"tasks": tasks})
 
 
-@bp_flasgger.route("/todo/api/v1.0/tasks/token", methods=["GET"])
+@bp_flasgger.route("/todo/api/v1.0/tasks/token" ,methods=["GET"])
 # @swag_from('yml/login_specs.yml', methods=['GET'])
 @auth.login_required
 def get_token():
     token = g.user.generate_auth_token()
     return jsonify({"token": token})
+
+@bp_flasgger.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["GET"])
+@auth.login_required
+def get_task(task_id):
+    query_task = Tasks.query.filter_by(task_id=task_id).first()
+    if query_task is None:
+        raise IdNotFoundException("Id not found")
+
+    return jsonify({"tasks": query_task.get_rep()})
+
+
+@bp_flasgger.route("/todo/api/v1.0/tasks", methods=["POST"])
+@auth.login_required
+def create_task():
+    if not request.json:
+        abort(400)
+    request_json = request.get_json(force=True)
+    parse_errors = iterate_properties_newtask(request_json)
+
+    if len(parse_errors) > 0:
+        raise JSONValidationError(parse_errors)
+
+    new_task = Tasks(
+        title=request_json["title"],
+        description=request_json["description"],
+        done=request_json["done"],
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({"task": new_task.get_rep()}), 201
+
+
+@bp_flasgger.route("/todo/api/v1.0/tasks", methods=["PUT"])
+@auth.login_required
+def update_task():
+
+    request_json = request.get_json(force=True)
+    parse_errors = iterate_properties_updatetask(request_json)
+
+    if len(parse_errors) > 0:
+        raise JSONValidationError(parse_errors)
+
+    task_to_update = Tasks.query.filter_by(task_id=request_json["id"]).first()
+    if task_to_update is None:
+        raise IdNotFoundException("Id not found")
+
+    task_to_update.done = request_json["done"]
+    if request_json.get("title"):
+        task_to_update.title = request_json["title"]
+    if request_json.get("description"):
+        task_to_update.description = request_json["description"]
+
+    db.session.commit()
+
+    return jsonify({"task": task_to_update.get_rep()})
+
+
+@bp_flasgger.route("/todo/api/v1.0/tasks", methods=["DELETE"])
+@auth.login_required
+def delete_task():
+
+    request_json = request.get_json(force=True)
+    parse_errors = iterate_properties_deletetask(request_json)
+
+    if len(parse_errors) > 0:
+        raise JSONValidationError(parse_errors)
+
+    task_to_delete = Tasks.query.filter_by(task_id=request_json["id"]).first()
+    if task_to_delete is None:
+        raise IdNotFoundException("Id not found")
+    task = task_to_delete.get_rep()
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return jsonify({"result": True, "task": task})
